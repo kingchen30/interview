8ca81af8674b5b9b13fbfc1711758a420a40e4c8
king <422688284@qq.com>
2019-10-31 00:05:14 +0800

    lengthOfLongestSubstring

---
+++ samples/LeetCode/src/LengthOfLongestSubstring.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class LengthOfLongestSubstring {
+  public static void main(String[] args) {
+    //
+  }
+  public static int lengthOfLongestSubstring(String s){
+      int ans = 0;
+      HashMap<Character, Integer> map = new HashMap<>();
+    for (int j = 0,i=0; j < s.length(); j++) {
+        if(map.containsKey(s.charAt(j))){
+            i = Math.max(map.get(s.charAt(j)), i);
+        }
+        ans = Math.max(ans, j-i+1);
+        map.put(s.charAt(j),j+1);
+    }
+      return ans;
+  }
+}
